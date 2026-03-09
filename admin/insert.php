80159f3dc205ed01f405c4f8955633ef81dcc0c4
Marko Damjanac <36675797+markodam5@users.noreply.github.com>
2026-03-09 10:07:28 +0100

    Add required fields and improve form layout

--- admin/insert.php
+++ admin/insert.php
@@ -11,9 +11,11 @@
 	}
 	?>
 	<form action="" method="post">
-		<p>Model: <br><input type="text" name="model"><p>
-		<p>Manu: <br><input type="text" name="manu"></p>
-		<input type="submit" name="submit" value="insert">
+		<p>Model: <br><input type="text" name="model" required></p>
+		<p>Manu: <br><input type="text" name="manu" required></p>
+		<div class="form-actions">
+			<input type="submit" name="submit" class="insert-new submit-inline" value="Insert New">
+			<a href="./" class="back-link">Back to the cars list</a>
+		</div>
 	</form> 
-	<a href="./" class="back-link">Back to the users list</a>
 </div>
